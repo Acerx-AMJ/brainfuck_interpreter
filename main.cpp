d32b913ea67378cec104009c2c7d1782e507fd9e
Acerx-AMJ <ajm.acerx@gmail.com>
2025-09-12 09:05:40 +0300

    Changed code to be in a single file.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,57 @@
-#include "brainfuck.hpp"
+#include <iostream>
 #include <fstream>
+#include <unordered_map>
 
 int main(int argc, char* argv[]){
    if (argc != 2) {
       std::cerr << "Expected two args, got " << argc << " instead.\n";
       return -1;
    }
-   std::string arg = argv[1];
-   std::ifstream file (arg);
+   std::string input = argv[1];
+   std::ifstream file (input);
 
    if (file.is_open()) {
-      arg.clear();
+      input.clear();
       std::string temp;
 
       while (std::getline(file, temp)) {
-         arg += temp + '\n';
+         input += temp + '\n';
       }      
    }
-   run_brainfuck(arg);
+   std::unordered_map<int, unsigned char> memory;
+   int pointer = 0;
+   
+   for (int i = 0; i < input.size(); ++i) {
+      const auto& ch = input.at(i);
+      if (ch == '>') {
+         ++pointer;
+      } else if (ch == '<') {
+         --pointer;
+      } else if (ch == '+') {
+         ++memory[pointer];
+      } else if (ch == '-') {
+         --memory[pointer];
+      } else if (ch == '.') {
+         std::cout << memory[pointer];
+      } else if (ch == ',') {
+         std:: cin >> std::noskipws >> memory[pointer];
+      } else if (ch == '[') {
+         if (memory[pointer] != 0) {
+            continue;
+         }
+         int depth = 1;
+         for (++i; i < input.size() && depth > 0; ++i) {
+            depth += (input.at(i) == '[') - (input.at(i) == ']');
+         }
+      } else if (ch == ']') {
+         if (memory[pointer] == 0) {
+            continue;
+         }
+         int depth = 1;
+         for (--i; i >= 0 && depth > 0; --i) {
+            depth += (input.at(i) == ']') - (input.at(i) == '[');
+         }
+      }
+   }
    return 0;
 }
